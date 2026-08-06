0c537bc1b737bf2f3876cbe78ce258358c8c51c0
shaina240506 <shaina.sharma1974@gmail.com>
2026-08-06 16:32:44 +0530

    Time: 0 ms (100%), Space: 42.1 MB (39.46%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int low=1;
+        int high=num;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            long square=(long)mid*mid;
+            if(square==num){
+                return true;
+            }
+            else if(square<num){
+                low=mid+1;
+
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return false;
+    }
+}
